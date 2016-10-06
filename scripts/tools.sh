38d2c76fbb1b4ce069f2dfc38cc2252ab96a0c3e
Peter H <peter.steffensen@gmail.com>
2016-10-06 21:30:06 +0200

    added radare2. scripts needs some cleaning up...

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -3,7 +3,15 @@
 git clone https://github.com/longld/peda.git ~/peda
 echo source ~/peda/peda.py >> ~/.gdbinit
 
-sudo pip install --upgrade capstone
-sudo pip install --upgrade ropgadget
+sudo pip install --upgrade capstone > /dev/null
+sudo pip install --upgrade ropgadget > /dev/null
 
-sudo pip install --upgrade pwntools
+sudo pip install --upgrade pwntools > /dev/null
+
+git clone https://github.com/radare/radare2.git ~/radare2
+pushd ~/radare2
+sudo ./configure
+sudo make
+sudo make install
+popd
+
