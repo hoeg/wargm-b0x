d5ceb7bca2809671f6a07680f7f35ae1e5312eba
Peter H <peter.steffensen@gmail.com>
2016-10-10 22:25:02 +0200

    added ropper

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/longld/peda.git ~/peda
+git clone https://github.com/longld/peda.git ~/peda > /dev/null
 echo source ~/peda/peda.py >> ~/.gdbinit
 
 sudo pip install --upgrade capstone > /dev/null
@@ -8,10 +8,14 @@ sudo pip install --upgrade ropgadget > /dev/null
 
 sudo pip install --upgrade pwntools > /dev/null
 
-git clone https://github.com/radare/radare2.git ~/radare2
+git clone https://github.com/radare/radare2.git ~/radare2 > /dev/null
 pushd ~/radare2
-sudo ./configure
-sudo make
-sudo make install
+sudo ./configure > /dev/null
+sudo make > /dev/null
+sudo make install > /dev/null
 popd
+
+#Ropper for ROP chains
+sudo pip install filebytes
+sudo pip install ropper
 
